e2ca8d55c9098cfdd4d1513db182316e3be811e7
anas-er25 <anaserrakibi@gmail.com>
2024-08-26 23:37:53 +0100

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
-  description: "Created with the help of Frontend Tribe",
+  title: "Anas Dev",
+  description: "Anas Dev is a portfolio",
 };
 
 export default function RootLayout({
