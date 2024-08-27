30e9af23c698de33c6d83a94ea8416fbc6b8f896
anas-er25 <anaserrakibi@gmail.com>
2024-08-27 02:08:10 +0100

    add header and hero

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -81,7 +81,7 @@ export const HeroSection = () => {
             <ArrowDown className="size-4" />
           </button>
           <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 px-6 h-12 rounded-xl">
-            <span className="font-semibold">Let's Connect</span>
+            <span className="font-semibold">Let&apos;s Connect</span>
           </button>
         </div>
       </div>
